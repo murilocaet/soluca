5273840b33d2b5bf1a3e0b10b9c15216dfa57676
MURILO CAETANO <MURILO CAETANO@10.0.0.9>
2017-02-05 20:46:00 -0300

    Ajustes

--- solucao/WebContent/js/app/app-controller.js
+++ solucao/WebContent/js/app/app-controller.js
@@ -7,73 +7,91 @@
 	function AppCtrl($scope, $window, AppService) { 
 		
 		var vmAppCtrl = this;
+		
+		$scope.errosload = [];
 
 		$scope.obterRotas = function(){
-			 AppService.obterRotas(vmAppCtrl.malhaDistribuicao).then(function(retorno){
-				 if(retorno.rotas != null || retorno.rotas != undefined){
-					 $scope.rotasProcessadas = [];
-					 if(retorno.rotas.length != undefined){
-						 for(var i= 0; i < retorno.rotas.length; i++){
-							 $scope.rotasProcessadas.push(retorno.rotas[i]);
+			$scope.errosload = [];
+			if(vmAppCtrl.malhaDistribuicao  != undefined){
+				AppService.obterRotas(vmAppCtrl.malhaDistribuicao).then(function(retorno){
+					 if(retorno.rotas != null || retorno.rotas != undefined){
+						 $scope.rotasProcessadas = [];
+						 if(retorno.rotas.length != undefined){
+							 for(var i= 0; i < retorno.rotas.length; i++){
+								 $scope.rotasProcessadas.push(retorno.rotas[i]);
+							 }
+						 }else{
+							 $scope.rotasProcessadas.push(retorno.rotas);
 						 }
-					 }else{
-						 $scope.rotasProcessadas.push(retorno.rotas);
 					 }
-				 }
-				 if(retorno.erros != null || retorno.erros != undefined){
-					 $scope.errosProcessados = [];
-					 if(retorno.erros.length != undefined){
-						 for(var i= 0; i < retorno.erros.length; i++){
-							 $scope.errosProcessados.push(retorno.erros[i]);
+					 if(retorno.erros != null || retorno.erros != undefined){
+						 $scope.errosProcessados = [];
+						 if(retorno.erros.length != undefined){
+							 for(var i= 0; i < retorno.erros.length; i++){
+								 $scope.errosProcessados.push(retorno.erros[i]);
+							 }
+						 }else{
+							 $scope.errosProcessados.push(retorno.erros);
 						 }
-					 }else{
-						 $scope.errosProcessados.push(retorno.erros);
 					 }
-				 }
-			 });
+				 });
+			}else{
+				vmAppCtrl.erro = {};
+				vmAppCtrl.erro.mensagem = "Nenhum arquivo selecionado!";
+				$scope.errosload.push(vmAppCtrl.erro);
+			}
 		 }
 		
 		
 		$scope.carregarMalhas = function(){
-			 AppService.carregarMalhas(vmAppCtrl.malha).then(function(retorno){
-				 
-				 if((retorno.malhas != null || retorno.malhas != undefined)){
-					 $scope.malhas = [];
-					 if(retorno.malhas.length != undefined){
-						 for(var i= 0; i < retorno.malhas.length; i++){
+			$scope.errosload = [];
+			if(vmAppCtrl.malha  != undefined){
+				AppService.carregarMalhas(vmAppCtrl.malha).then(function(retorno){
+					 
+					 if((retorno.malhas != null || retorno.malhas != undefined)){
+						 $scope.malhas = [];
+						 if(retorno.malhas.length != undefined){
+							 for(var i= 0; i < retorno.malhas.length; i++){
+								 vmAppCtrl.malha = {};
+								 vmAppCtrl.malha.nome = retorno.malhas[i].nome;
+								 vmAppCtrl.malha.rotas = [];
+								 if(retorno.malhas[i].rotas.length != undefined){
+									 for(var j= 0; j < retorno.malhas[i].rotas.length; j++){
+										 vmAppCtrl.malha.rotas.push(retorno.malhas[i].rotas[j]);
+									 }
+								 }else{
+									 vmAppCtrl.malha.rotas.push(retorno.malhas[i].rotas);
+								 }
+								 $scope.malhas.push(vmAppCtrl.malha);
+							 }
+						 }else{
 							 vmAppCtrl.malha = {};
-							 vmAppCtrl.malha.nome = retorno.malhas[i].nome;
+							 vmAppCtrl.malha.nome = retorno.malhas.nome;
 							 vmAppCtrl.malha.rotas = [];
-							 if(retorno.malhas[i].rotas.length != undefined){
-								 for(var j= 0; j < retorno.malhas[i].rotas.length; j++){
-									 vmAppCtrl.malha.rotas.push(retorno.malhas[i].rotas[j]);
+								
+							 if(retorno.malhas.rotas.length != undefined){
+								 for(var i= 0; i < retorno.malhas.rotas.length; i++){
+									 vmAppCtrl.malha.rotas.push(retorno.malhas.rotas[i]);
 								 }
 							 }else{
-								 vmAppCtrl.malha.rotas.push(retorno.malhas[i].rotas);
+								 vmAppCtrl.malha.rotas.push(retorno.malhas.rotas);
 							 }
 							 $scope.malhas.push(vmAppCtrl.malha);
 						 }
-					 }else{
-						 vmAppCtrl.malha = {};
-						 vmAppCtrl.malha.nome = retorno.malhas.nome;
-						 vmAppCtrl.malha.rotas = [];
-							
-						 if(retorno.malhas.rotas.length != undefined){
-							 for(var i= 0; i < retorno.malhas.rotas.length; i++){
-								 vmAppCtrl.malha.rotas.push(retorno.malhas.rotas[i]);
-							 }
-						 }else{
-							 vmAppCtrl.malha.rotas.push(retorno.malhas.rotas);
-						 }
-						 $scope.malhas.push(vmAppCtrl.malha);
 					 }
-				 }
-			 });
+				 });
+			}else{
+				vmAppCtrl.erro = {};
+				vmAppCtrl.erro.mensagem = "Nenhum arquivo selecionado!";
+				$scope.errosload.push(vmAppCtrl.erro);
+			}
+			
+			 
 		 }
 		
 		
 		$scope.mapaUploadedFile = function(element) {
-			
+			$scope.errosload = [];
 			var reader = new FileReader();
 			reader.onload = function () {
 				var malhasJson = reader.result;
@@ -86,12 +104,20 @@
 				vmAppCtrl.files = element.files;
 			});
 			
-			reader.readAsText(element.files[0]);
+			var nome = vmAppCtrl.files[0].name;
+			if(nome.substring(nome.length-5,nome.length) == ".json"){
+				reader.readAsText(element.files[0]);
+			}else{
+				vmAppCtrl.erroLoad = {};
+				vmAppCtrl.erroLoad.mensagem = "Arquivo inválido!";
+				$scope.errosload.push(vmAppCtrl.erroLoad);
+				$scope.$apply();
+			}
 		}
 		
 		
 		$scope.rotaUploadedFile = function(element) {
-			
+			$scope.errosload = [];
 			var reader = new FileReader();
 			reader.onload = function () {
 				var rotasJson = reader.result;
@@ -104,7 +130,16 @@
 				vmAppCtrl.files = element.files;
 			});
 			
-			reader.readAsText(element.files[0]);
+			var nome = vmAppCtrl.files[0].name;
+			if(nome.substring(nome.length-5,nome.length) == ".json"){
+				reader.readAsText(element.files[0]);
+			}else{
+				vmAppCtrl.erroLoad = {};
+				vmAppCtrl.erroLoad.mensagem = "Arquivo inválido!";
+				$scope.errosload.push(vmAppCtrl.erroLoad);
+				$scope.$apply();
+			}
+			
 		}
 		
 	}
